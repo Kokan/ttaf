aa69fa09b11acd960980457d28469f26181f6c0f
Kokan <kokaipeter@gmail.com>
2021-03-21 10:49:22 +0100

    extract java path and args
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- giraffe.sh
+++ giraffe.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+JAVA=/usr/lib/jvm/java-8-openjdk-amd64/bin/java
+JAVA_ARG="${JAVA_ARG} -agentlib:hprof=cpu=samples,file=../java.hprof.txt"
+
 cd giraffe
 gradle clean jar copyDependencies
 if [ "0" != "$?" ]; then
 	exit 1
 fi
-/usr/lib/jvm/java-8-openjdk-amd64/bin/java -agentlib:hprof=cpu=samples,file=../java.hprof.txt -cp "build/libs/*" dog.giraffe.WebcamFrame
+${JAVA} ${JAVA_ARG} -cp "build/libs/*" dog.giraffe.WebcamFrame
