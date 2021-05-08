521a3d8d794ec38703148218ffa3fb9dffc29309
Peter Nagy <a@b.c>
2021-05-08 22:22:13 +0200

    add generated javadoc

---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"dog.giraffe"},{"l":"dog.giraffe.cluster"},{"l":"dog.giraffe.gui"},{"l":"dog.giraffe.gui.model"},{"l":"dog.giraffe.image"},{"l":"dog.giraffe.image.transform"},{"l":"dog.giraffe.points"},{"l":"dog.giraffe.threads"},{"l":"dog.giraffe.util"}]
